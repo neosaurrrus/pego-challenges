0ac3d028cd7681a20a47a93c1d0df2b415dfd8d9
Lukie Kang <lukiek@gmail.com>
2022-11-28 00:56:41 +0000

    revert

--- hacker-news/components/HackerNews.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HackerNews() {
-  return (
-    <div>
-      <h1>Hacker News Logged in Placeholder</h1>
-    </div>
-  );
-}
--- hacker-news/pages/hacker-news/index.tsx
+++ hacker-news/pages/hacker-news/index.tsx
@@ -1,15 +1,43 @@
-import { useUser } from "@auth0/nextjs-auth0";
 import styles from "styles/Home.module.css";
-import HackerNews from "../../components/HackerNews";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import StoryGroup from "../../components/StoryGroup";
 
-function HackerNewsPage() {
-  const { user, isLoading, error } = useUser();
+type Props = {
+  askStories: number[];
+  showStories: number[];
+  jobStories: number[];
+};
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  return <div className={styles.container}>{user && <HackerNews />}</div>;
+function HackerNewsPage({ askStories, showStories, jobStories }: Props) {
+  return (
+    <div className={styles.container}>
+      <h2>Ask Stories</h2>
+      <StoryGroup storiesArray={askStories} />
+      <h2>Show Stories</h2>
+      <StoryGroup storiesArray={showStories} />
+      <h2>Job Stories</h2>
+      <StoryGroup storiesArray={jobStories} />
+    </div>
+  );
 }
 
 export default withPageAuthRequired(HackerNewsPage);
+
+export async function getStaticProps() {
+  const [askRes, showRes, jobRes] = await Promise.all([
+    fetch("https://hacker-news.firebaseio.com/v0/askstories.json"),
+    fetch("https://hacker-news.firebaseio.com/v0/showstories.json"),
+    fetch("https://hacker-news.firebaseio.com/v0/jobstories.json"),
+  ]);
+
+  const [askStories, showStories, jobStories] = await Promise.all([
+    askRes.json(),
+    showRes.json(),
+    jobRes.json(),
+  ]);
+
+  return {
+    props: { askStories, showStories, jobStories },
+    revalidate: 60,
+  };
+}
