f5768ecb1826366c807624579cb50e42007d2f1f
Lukie Kang <lukiek@gmail.com>
2022-11-29 09:30:28 +0000

    feat: style base layout

--- hacker-news/pages/index.tsx
+++ hacker-news/pages/index.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-1 overflow-y-auto h-4/5 items-center justify-center">
-      <h1 className="text-4xl">
-        <a href="/hacker-news" className="text-blue-400">
+    <div className="flex flex-col flex-1 overflow-y-auto h-4/5 items-center justify-center">
+      <div className="text-[70px]">📰</div>
+      <h1 className="text-4xl flex flex-col gap-4 items-center">
+        Hackernews App
+        <a href="/hacker-news" className="text-blue-400 text-lg">
           Login
         </a>{" "}
-        to see Hacker News
       </h1>
     </div>
   );
